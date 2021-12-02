c1b4e08a53f00c860c13d68c753c9bbc4a1b8b96
Botkik-hub <stas.voronov.91@mail.ru>
2021-12-01 20:35:58 -0500

    Added doge roll
    raw version of doge roll was added

--- Assets/Scripts/Movment.cs
+++ Assets/Scripts/Movment.cs
@@ -5,25 +5,44 @@ using UnityEngine;
 public class Movment : MonoBehaviour
 {
     Rigidbody2D body;
+    Transform transform;
 
     float horizontal;
     float vertical;
 
     public float runSpeed = 10.0f;
+    public float dogeSpeed = 500f;
 
+    private bool doDoge = false;
+    
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        transform = GetComponent<Transform>();
     }
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            doDoge = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        if (doDoge)
+        {
+            body.velocity = new Vector2(horizontal * dogeSpeed, vertical * dogeSpeed);
+            doDoge = false;
+        }
+        else
+        {
+            body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        }
+        
+
     }
 }
