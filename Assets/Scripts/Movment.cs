e48e325b75df2dcba8be6754d607f9d528d390a6
Botkik-hub <stas.voronov.91@mail.ru>
2021-12-02 14:34:10 -0500

    Added goge roll again

--- Assets/Scripts/Movment.cs
+++ Assets/Scripts/Movment.cs
@@ -10,6 +10,9 @@ public class Movment : MonoBehaviour
     float vertical;
 
     public float runSpeed = 10.0f;
+    public float dogeSpeed = 500f;
+
+    private bool doDoge = false;
 
     void Start()
     {
@@ -20,10 +23,25 @@ public class Movment : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            doDoge = true;
+        }
     }
 
     private void FixedUpdate()
     {
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        if (doDoge)
+        {
+            body.velocity = new Vector2(horizontal * dogeSpeed, vertical * dogeSpeed);
+            doDoge = false;
+        }
+        else
+        {
+            body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        }
+
+
     }
 }
